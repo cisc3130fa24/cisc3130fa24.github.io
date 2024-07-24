c9827053d9f0efff2385a971f5fd56e990bd8a14
molach <moshe.lach@yahoo.com>
2024-07-23 22:46:50 -0400

    updates

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function toggleCollapsible(header) {
     const content = header.nextElementSibling;
     const icon = header.querySelector('.icon');
+    
     if (content.style.display === 'block') {
         content.style.display = 'none';
         icon.style.transform = 'rotate(0deg)';
